006f3c7c3c6bcc868caf919ef69e0ff2fcfae4af
Natalia <nat.kaseeva@gmail.com>
2022-04-25 10:37:36 -0600

    add items and delete items functionality was added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -11,34 +11,79 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];
-const workItems = [];
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true});
+
+const itemsSchema = {
+  name:String
+};
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name:"Welcome to your ToDoList!"
+});
+
+const item2 = new Item({
+  name:"Hit the + button to add a new item."
+});
+
+const item3 = new Item({
+  name:"Hit the + button to delete an item."
+});
+
+const defaultItems = [item1, item2, item3];
+
+
 
 app.get("/", function(req, res) {
 
-const day = date.getDate();
+  Item.find({}, function(err, foundItems) {
+    
+    if(foundItems.length === 0) {
+      Item.insertMany(defaultItems, function(err){
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved default items to DB.")
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {listTitle: "Today", newListItems: foundItems});
+    }
 
-  res.render("list", {listTitle: day, newListItems: items});
-
+  });
 });
+
 
 app.post("/", function(req, res){
+  const itemName = req.body.newItem;
 
-  const item = req.body.newItem;
+  const item = new Item({
+    name: itemName
+  });
 
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  item.save();
+  res.redirect("/");
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.post("/delete", function(req, res){
+ const checkedItemId = req.body.checkbox;
+
+ Item.findByIdAndRemove(checkedItemId, function(err){
+   if(!err) {
+     console.log("Successfully deleted checked item.");
+   }
+
+   res.redirect("/");
+ });
 });
 
+app.get("/:customListName", function(req, res){
+  const customListName = req.params.customListName;
+
+  
+});
 
 
 app.listen(3000, function() {
